0da7525c52d6be381d61a548ded1cc813cb03f12
TuyisengeSulaiman <157732522+TuyisengeSulaiman@users.noreply.github.com>
2024-10-13 16:42:42 +0200

    Update page.tsx

--- app/bus/page.tsx
+++ app/bus/page.tsx
@@ -5,7 +5,9 @@ export default function Page() {
         <main className='flex flex-col gap-8 w-full md:w-[700px]'>
           <article>
 
-            Bus
+            
+            <h1>Bus</h1
+            <p>Le service de bus scolaire est assuré  le matin à partir de 7h10 ,à midi à partir de 12h10 et à partir de 16h à la sortie de classe</p>
 
           </article>
         </main>
