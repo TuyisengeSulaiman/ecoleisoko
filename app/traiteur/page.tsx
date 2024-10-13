fad72319dda6c5e4791fb6f5793d40c2cbd3453e
TuyisengeSulaiman <157732522+TuyisengeSulaiman@users.noreply.github.com>
2024-10-13 16:02:29 +0200

    Update page.tsx

--- app/traiteur/page.tsx
+++ app/traiteur/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
           <article>
 
             <h1 className='text-4xl font-bold mb-4'>Traiteur</h1>
-            <p>Vous pouvez consulter <a className="text-blue-500 hover:underline" href="https://Isoko la Source.ecoleouestmtl.com/wp-content/uploads/2022/09/Infolettre-Sept2022.pdf">quelques informations</a> à propos du traiteur scolaire. Si vous souhaitez commander ou voir le menu, vous pouvez vous rendre au <a href="https://www.traiteurmerenda.com/accueil.html">https://www.traiteurmerenda.com/</a>.</p>
+            <p>Vous pouvez consulter <a className="text-blue-500 hover:underline" href="https://Isoko la Source.ecoleouestmtl.com/wp-content/uploads/2022/09/Infolettre-Sept2022.pdf">quelques informations</a> à propos du traiteur scolaire. Si vous souhaitez commander ou voir le menu, vous pouvez contacter Colibri Taverne au Numéro +250 789 339 672
             <ul>
               <li><a className="text-blue-500 hover:underline" href="https://Isoko la Source.ecoleouestmtl.com/wp-content/uploads/2023/10/Infolettre_Merenda_octobre.pdf">Infolettre Merenda – Octobre 2023</a></li>
             </ul>
