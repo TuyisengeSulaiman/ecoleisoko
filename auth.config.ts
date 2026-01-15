61ab30356e6a77502d27c22dd545b2a6d7d3cba2
DivinPrince <dpcode36@gmail.com>
2026-01-15 16:23:37 +0200

    feat: implement authentication with credentials provider and update Next.js version

--- auth.config.ts
+++ auth.config.ts
@@ -1,34 +1,8 @@
-import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import { getUserByEmail } from '@/lib/data';
-import { loginSchema } from '@/lib/validations';
 
 export default {
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const validatedFields = loginSchema.safeParse(credentials);
-        
-        if (validatedFields.success) {
-          const { email, password } = validatedFields.data;
-
-          const user = await getUserByEmail(email);
-          if (!user || !user.passwordHash) return null;
-
-          const passwordsMatch = await bcrypt.compare(password, user.passwordHash);
-
-          if (passwordsMatch) return {
-            email: user.email,
-            name: user.name || "Admin",
-            id: user.id,
-            role: "admin",
-            image: user.image,
-          };
-        }
-
-        return null;
-      },
-    }),
-  ],
+  providers: [],
+  pages: {
+    signIn: '/admin/login',
+  },
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,12 +1,39 @@
+import bcrypt from 'bcryptjs';
 import NextAuth from "next-auth";
+import Credentials from 'next-auth/providers/credentials';
+import { getUserByEmail } from '@/lib/data';
+import { loginSchema } from '@/lib/validations';
 import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  ...authConfig,
   session: {
     strategy: "jwt",
   },
-  pages: {
-    signIn: '/admin/login',
-  },
-  ...authConfig
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = loginSchema.safeParse(credentials);
+        
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.passwordHash) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.passwordHash);
+
+          if (passwordsMatch) return {
+            email: user.email,
+            name: user.name || "Admin",
+            id: user.id,
+            role: "admin",
+            image: user.image,
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
 });
